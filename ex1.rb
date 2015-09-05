a32d054b31d91dcc5f5de752859c365a2429cbc5
zacscodingclub <zbaston@gmail.com>
2015-09-05 21:11:40 +0000

    First 5 exercices complete.

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+puts "Hello World!"
+puts "Hello again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+# puts "i'd must rather you 'not'."
+puts 'I "said" do not touch this.'
---
+++ ex5.rb
@@ -0,0 +1,17 @@
+name = 'Zac Baston'
+age = 30
+height = 70 # inches
+weight = 205.5 # lbs
+eyes = 'Green'
+teeth = 'Coffee'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches/#{height*2.54}cm tall."
+puts "He's #{weight} pounds/#{weight * 2.2} kilograms heavy."
+puts "Actually, that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} colored."
+
+puts "If I add #{age}, #{height}, and #{weight} together, I get #{age + height + weight}."
+

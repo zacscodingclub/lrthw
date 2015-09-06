99e5a0214ee20d0da7901143d70f4bde4af8e29a
zacscodingclub <zbaston@gmail.com>
2015-09-06 20:15:51 +0000

    Exercises 5-10 completed.

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabb_cat = "\tI'm tabbed in."
+persion_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list: 
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabb_cat
+puts persion_cat
+puts backslash_cat
+puts fat_cat
